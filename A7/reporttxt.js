903276ff452409ca66a378be73d455b95f076a0a
beretverde <tylerjacox@gmail.com>
2017-07-09 12:38:19 -0600

    A7 Starter

---
+++ A7/reporttxt.js
@@ -0,0 +1,33 @@
+/*
+
+   Author:   
+   Date:     
+
+   Filename: report.js
+
+
+
+   Functions List:
+
+   initPage()
+      Initializes the contents of the Web page
+
+   testLength()
+      Tests a field for its length
+
+   testPattern()
+      Tests a field for its pattern
+
+   validateForm
+      Validates a Web form
+
+   calcRow
+      Calculates the costs within one row of the travel report
+
+   calcTotal
+      Calculates the total cost of the travel
+
+   upDate
+      Updates the total travel cost
+*/
+
